89ebaa330d3a3f7d07d01fab115aa4a53d3dcba0
chengwl <1015372312@qq.com>
2018-02-09 18:11:39 +0800

    aa

--- app/Http/Middleware/CheckBlogAuthor.php
+++ app/Http/Middleware/CheckBlogAuthor.php
@@ -19,6 +19,9 @@ class CheckBlogAuthor
         if (Auth::check()) {
             $user= Auth::user();
             if(empty($user->blog_id)){
+		 if(strtolower($request->path())== 'blog/applyform'|strtolower($request->path())== 'blog/apply'){
+                    return $next($request);
+                }
                 return  redirect(url('blog/applyform'));
                 // abort(403, '您不是专栏作者,无权访问该页面!');//后期跳到申请专栏
             }
